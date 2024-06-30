34ec5d76ed9ec4df39eb7f40b71cb94d6bc86f46
lukas <lucasmachado292@gmail.com>
2024-06-30 13:48:04 -0300

    melhoras na visualização do tabuleiro

--- src/jogodavelha/Tabuleiro.java
+++ src/jogodavelha/Tabuleiro.java
@@ -16,16 +16,16 @@ public class Tabuleiro {
         int i;
         for (i = 0; i < tamanho; i++) {
             char ch = (char) (i + 65);
-            System.out.print(ch);
+            System.out.print(ch + " ");
             System.out.print(" ");
             for (int j = 0; j < tamanho; j++) {
                 System.out.print(matriz[i][j]);
-                if (j < tamanho - 1) System.out.print("|");
+                if (j < tamanho - 1) System.out.print(" | ");
             }
             System.out.println();
-            if (i < tamanho - 1) System.out.println("  -----");
+            if (i < tamanho - 1) System.out.println("  -----------");
         }
-        System.out.println("  0 1 2");
+        System.out.println("   0   1   2");
     }
     public void receberJogada(Jogada jogada){
         this.matriz[jogada.getX()][jogada.getY()]= jogada.getSimbolo();
